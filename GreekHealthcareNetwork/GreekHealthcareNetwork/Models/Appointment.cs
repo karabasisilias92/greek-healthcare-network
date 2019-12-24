a23e8b8f9cb63d364f106ee4391c2647c16cec1b
tsantirakieva1581 <tsantiraki.evangelia@gmail.com>
2019-12-24 12:58:13 +0200

    New Commit

---
+++ GreekHealthcareNetwork/GreekHealthcareNetwork/Models/Appointment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreekHealthcareNetwork.Models
+{
+    public class Appointment
+    {
+    }
+}
--- GreekHealthcareNetwork/GreekHealthcareNetwork/Models/InsuredPlan.cs
+++ GreekHealthcareNetwork/GreekHealthcareNetwork/Models/InsuredPlan.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GreekHealthcareNetwork.Models
 {
-    class InsuredPlan
+    public class InsuredPlan
     {
         [Key]
         public int Id { get; set; }
