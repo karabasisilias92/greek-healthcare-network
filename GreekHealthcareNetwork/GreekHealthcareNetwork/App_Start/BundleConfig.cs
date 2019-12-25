0ba86023c0e3eda6de6795487dc846f69e7616a9
karabasisilias92 <karabasis.ilias@gmail.com>
2019-12-25 03:25:06 +0200

    First steps for visitors landing page. Incorporated relative css and js files.

--- GreekHealthcareNetwork/GreekHealthcareNetwork/App_Start/BundleConfig.cs
+++ GreekHealthcareNetwork/GreekHealthcareNetwork/App_Start/BundleConfig.cs
@@ -9,10 +9,22 @@ namespace GreekHealthcareNetwork
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-migrate.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/regnaLibs").Include(
+                        "~/Scripts/RegnaScripts/libs/easing.min.js",
+                        "~/Scripts/RegnaScripts/libs/wow.min.js",
+                        "~/Scripts/RegnaScripts/libs/waypoints.min.js",
+                        "~/Scripts/RegnaScripts/libs/counterup.min.js",
+                        "~/Scripts/RegnaScripts/libs/hoverIntent.js",
+                        "~/Scripts/RegnaScripts/libs/superfish.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/regnaScripts").Include(
+                        "~/Scripts/RegnaScripts/main.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -24,6 +36,8 @@ namespace GreekHealthcareNetwork
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/animate.min.css",
+                      "~/Content/font-awesome.min.css",
                       "~/Content/site.css"));
         }
     }
