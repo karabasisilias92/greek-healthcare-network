b0fcd91cd69cebac797ec365d3b2b1d3ca9efe70
tsantirakieva1581 <tsantiraki.evangelia@gmail.com>
2019-12-28 17:14:41 +0200

    Changes in DoctorsListPartial

---
+++ GreekHealthcareNetwork/GreekHealthcareNetwork/Controllers/MessagesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GreekHealthcareNetwork.Controllers
+{
+    public class MessagesController : ApiController
+    {
+        //tha stelnei sync ta mnm sth vash
+        //ena post action
+    }
+}
