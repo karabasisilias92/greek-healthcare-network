0503ae69da131e8be5148b0bba0088fadb1b0a14
tsantirakieva1581 <tsantiraki.evangelia@gmail.com>
2019-12-29 20:54:24 +0200

    Message View

---
+++ GreekHealthcareNetwork/GreekHealthcareNetwork/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using GreekHealthcareNetwork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Services.Description;
+
+namespace GreekHealthcareNetwork.Controllers
+{
+    public class MessageController : ApiController
+    {
+        private readonly MessagesRepository _message = new MessagesRepository();
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            return Ok(_message.FindById(id));
+        }
+        [HttpPost]
+        public IHttpActionResult Post([FromBody]Message message)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            _message.Add(message);
+            return ResponseMessage(Request.CreateResponse(HttpStatusCode.Created));
+            //h return Ok();
+        }
+    }
+}
---
+++ GreekHealthcareNetwork/GreekHealthcareNetwork/Models/MessagesRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreekHealthcareNetwork.Models
+{
+    public class MessagesRepository
+    {
+        private static readonly List<Message> _message = new List<Message>()
+        {
+            new Message
+            {
+                Id = 1,
+            },
+            new Message
+            {
+                Id=2
+            }
+
+        };
+
+        public Message FindById(int id)
+        {
+            return _message.SingleOrDefault(i => i.Id == id);
+        }
+        public void Add(Message message)
+        {
+            //15o vhma
+            int newId = _message.Max(i => i.Id) + 1;
+            message.Id = newId;
+            _message.Add(message);
+        }
+    }
+}
