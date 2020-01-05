db2d1b20e822eb08b6e1d6120d03971382987822
tsantirakieva1581 <tsantiraki.evangelia@gmail.com>
2020-01-05 21:40:55 +0200

    ReplyMessage

--- GreekHealthcareNetwork/GreekHealthcareNetwork/Repositories/MessagesRepository.cs
+++ GreekHealthcareNetwork/GreekHealthcareNetwork/Repositories/MessagesRepository.cs
@@ -33,6 +33,19 @@ namespace GreekHealthcareNetwork.Repositories
                 db.SaveChanges();
             }      
         }
+        public void UpdateMessage(Message message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException("message");
+            }
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                db.Messages.Attach(message);
+                db.Entry(message).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
         public IEnumerable<Message> GetAllMessages(string UserId)
         {
             IEnumerable<Message> messages;
@@ -43,6 +56,8 @@ namespace GreekHealthcareNetwork.Repositories
             using (ApplicationDbContext db = new ApplicationDbContext())
             {
                 messages = db.Messages.Where(m => m.RecipientId.Equals(UserId) || m.SenderId.Equals(UserId))
+                                  .OrderByDescending(m => m.SentDate)
+                                  .ThenByDescending(m => m.SentTime)
                                   .Include("Sender")
                                   .Include("Sender.Roles")
                                   .Include("Recipient")
