fc990d0f3d92783a637088ecaef0025323878a04
karabasisilias92 <karabasis.ilias@gmail.com>
2020-01-09 23:17:34 +0200

    Add class menu-active to current page link on navbars. Minor fix to automated message created when appointment is cancelled.

---
+++ GreekHealthcareNetwork/GreekHealthcareNetwork/Scripts/setNavItemActive.js
@@ -0,0 +1,37 @@
+﻿function main() {
+    var title = $('title').text();
+    var li = $('ul.nav-menu > li');
+    if (title.includes('Home')) {
+        for (let i = 0; i < li.length; i++) {
+            if (li[i].innerText.includes('HOME')) {
+                li[i].classList.add('menu-active');
+                break;
+            }
+        }
+    }
+    else if (title.includes('Doctors')) {
+        for (let i = 0; i < li.length; i++) {
+            if (li[i].innerText.includes('DOCTORS')) {
+                li[i].classList.add('menu-active');
+                break;
+            }
+        }
+    }
+    else if (title.includes('Book Appointment')) {
+        for (let i = 0; i < li.length; i++) {
+            if (li[i].innerText.includes('BOOK APPOINTMENT')) {
+                li[i].classList.add('menu-active');
+                break;
+            }
+        }
+    }
+    else if (title.includes('Appointments') && !title.includes('Appointments History') ) {
+        for (let i = 0; i < li.length; i++) {
+            if (li[i].innerText.includes('APPOINTMENTS')) {
+                li[i].classList.add('menu-active');
+                break;
+            }
+        }
+    }
+}
+window.addEventListener('load', main);
