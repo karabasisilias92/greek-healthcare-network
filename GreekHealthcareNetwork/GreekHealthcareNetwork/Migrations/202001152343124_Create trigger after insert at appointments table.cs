a1d60d18d230e22d5c273bf9b349825d955ba5ce
karabasisilias92 <karabasis.ilias@gmail.com>
2020-01-16 04:38:52 +0200

    Created trigger and procedure for deleting unpaid appointments

---
+++ GreekHealthcareNetwork/GreekHealthcareNetwork/Migrations/202001152343124_Create trigger after insert at appointments table.cs
@@ -0,0 +1,52 @@
+namespace GreekHealthcareNetwork.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Createtriggerafterinsertatappointmentstable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(
+                @"
+                CREATE TRIGGER CallDeleteAppointmentProcedureAfterInsert ON Appointments
+                AFTER INSERT, UPDATE AS
+                BEGIN
+                    SET NOCOUNT ON;
+	                DECLARE @Id INT;
+                    SELECT @Id = Id FROM inserted;  
+	                DECLARE @jobname NVARCHAR(MAX); -- job name
+	                DECLARE @command NVARCHAR(MAX); -- command
+
+	                SET @jobname = CONCAT('DeleteIfAppointmentIsNotPaidOnTime', @Id);
+	                SET @command = CONCAT('EXEC DeleteIfAppointmentIsNotPaidOnTime @AppointmentId = ', @Id);
+
+	                exec msdb.dbo.sp_add_job
+	                @job_name = @jobname,
+	                @enabled=1,
+	                @start_step_id=1,
+	                @delete_level=1 --Job will delete itself after success
+
+	                exec msdb.dbo.sp_add_jobstep
+	                @job_name=@jobname,
+	                @step_id=1,
+	                @step_name='step1',
+	                @command=@command
+
+	                exec msdb.dbo.sp_add_jobserver
+	                @job_name = @jobname
+
+	                exec msdb.dbo.sp_start_job
+	                @job_name=@jobname
+                END;"
+            );
+        }
+        
+        public override void Down()
+        {
+            Sql(@"
+                DROP TRIGGER IF EXISTS CallDeleteAppointmentProcedureAfterInsert;
+            ");
+        }
+    }
+}
