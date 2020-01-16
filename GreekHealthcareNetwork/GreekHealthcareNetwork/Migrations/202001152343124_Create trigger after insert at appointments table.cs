596425d15cac421468cc1d18356cb1c9f73a2c19
karabasisilias92 <karabasis.ilias@gmail.com>
2020-01-16 05:18:23 +0200

    Fixes

--- GreekHealthcareNetwork/GreekHealthcareNetwork/Migrations/202001152343124_Create trigger after insert at appointments table.cs
+++ GreekHealthcareNetwork/GreekHealthcareNetwork/Migrations/202001152343124_Create trigger after insert at appointments table.cs
@@ -17,24 +17,28 @@ namespace GreekHealthcareNetwork.Migrations
                     SELECT @Id = Id FROM inserted;  
 	                DECLARE @jobname NVARCHAR(MAX); -- job name
 	                DECLARE @command NVARCHAR(MAX); -- command
-
+                    
 	                SET @jobname = CONCAT('DeleteIfAppointmentIsNotPaidOnTime', @Id);
-	                SET @command = CONCAT('EXEC DeleteIfAppointmentIsNotPaidOnTime @AppointmentId = ', @Id);
+	                SET @command = CONCAT('EXEC dbo.DeleteIfAppointmentIsNotPaidOnTime @AppointmentId = ', @Id);
 
 	                exec msdb.dbo.sp_add_job
 	                @job_name = @jobname,
 	                @enabled=1,
+                    @owner_login_name = 'sa',
 	                @start_step_id=1,
 	                @delete_level=1 --Job will delete itself after success
 
 	                exec msdb.dbo.sp_add_jobstep
 	                @job_name=@jobname,
+                    @database_name = 'GreekHealthcareNetwork',
+                    @subsystem = 'TSQL',
 	                @step_id=1,
 	                @step_name='step1',
 	                @command=@command
 
 	                exec msdb.dbo.sp_add_jobserver
-	                @job_name = @jobname
+	                @job_name = @jobname,
+                    @server_name = N'(LOCAL)'
 
 	                exec msdb.dbo.sp_start_job
 	                @job_name=@jobname
